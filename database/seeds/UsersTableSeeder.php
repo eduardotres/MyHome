238e9351fb7c5c1244ff0f42e4b95ca4d91e0155
Eduardo A.Tres de Souza <devtres19@gmail.com>
2020-08-29 14:30:33 -0300

    upando por github

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,7 +12,7 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         $user =  User::create([
-            'name' => 'Admin',
+            'name' => 'Usuario',
             'email' => 'admin@demo.com',
             'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
             'remember_token' => str_random(10),
